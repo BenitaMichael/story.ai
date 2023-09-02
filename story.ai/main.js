45192f73e35f9ee8eb6307c27fbbebd1c104c224
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2023-09-02 23:35:50 +0100

    Images

--- story.ai/main.js
+++ story.ai/main.js
@@ -1,6 +1,34 @@
 const mobile = document.getElementById("menu");
-const navList = document.getElementById("hidden-menu")
+const navList = document.getElementById("hidden-menu");
+
+const firstHeaderImage = document.getElementById("header-img");
+const secondHeaderImage = document.getElementById("header-img1");
 
 mobile.addEventListener("click", () =>{
     navList.classList.toggle("active");
 });
+
+updateImages = () =>{
+    window.addEventListener("load", ()=>{
+        toggleImage = () =>{
+            if (firstHeaderImage.src.includes("Property_1_Variant1.png")) {
+                firstHeaderImage.src = "images/Modern cute chatbot illustration set 3.png";
+            } else {
+                firstHeaderImage.src = "images/Property_1_Variant1.png";
+            }
+        }
+
+        imageToggle = () =>{
+            if(secondHeaderImage.src.includes("Property_1_Variant2.png")){
+                secondHeaderImage.src = "images/Modern cute chatbot illustration set 4.png"
+            }else{
+                secondHeaderImage.src = "images/Property_1_Variant2.png"
+            }
+        }
+
+        setInterval(toggleImage, 1000);
+        setInterval(imageToggle, 1000)
+
+    })
+}
+updateImages();
