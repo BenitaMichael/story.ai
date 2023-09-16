eb979dea11c803c0e73fc295d7362bcf59752a5c
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2023-09-16 21:15:49 +0100

    Made Js code cleaner

--- story.ai/main.js
+++ story.ai/main.js
@@ -1,88 +1,75 @@
-//Navbar responsiveness
+// Navbar responsiveness
 const mobile = document.getElementById("menu");
 const navList = document.getElementById("hidden-menu");
 
-//Updating images
+// Updating images
 const firstHeaderImage = document.getElementById("header-img");
 const secondHeaderImage = document.getElementById("header-img1");
 
-//Blog slides
+// Blog slides
 const first = document.getElementById("first");
-const second = document.getElementById("second")
+const second = document.getElementById("second");
 const vl = document.getElementById("vl");
 
-mobile.addEventListener("click", () =>{
-    navList.classList.toggle("active");
+// Function to toggle navigation menu
+mobile.addEventListener("click", () => {
+  navList.classList.toggle("active");
 });
 
-updateImages = () =>{
-    window.addEventListener("load", ()=>{
-        //Changing images
-        toggleImage = () =>{
-            if (firstHeaderImage.src.includes("Property_1_Variant1.png")) {
-                firstHeaderImage.src = "images/Modern cute chatbot illustration set 3.png";
-            } else {
-                firstHeaderImage.src = "images/Property_1_Variant1.png";
-            }
-        }
-
-        imageToggle = () =>{
-            if(secondHeaderImage.src.includes("Property_1_Variant2.png")){
-                secondHeaderImage.src = "images/Modern cute chatbot illustration set 4.png"
-            }else{
-                secondHeaderImage.src = "images/Property_1_Variant2.png"
-            }
-        }
-
-        setInterval(toggleImage, 1000);
-        setInterval(imageToggle, 1000)
-
-    })
-
-    //Moving line
-    moveRight= () =>{
-        let position = 0;
-     
-        animate = () =>{
-         if (position == 230){
-             clearInterval(animation);
-         }else{
-             position ++;
-             vl.style.left = position + "px";
-         }
-        }
-        let animation = setInterval(animate, 15);
-
-     
-    
-    
-        //Works with appearing texts
-         const duration = 2000; 
-        const increment = 0.01;
-    
-        let opacity = 0;
-
-        const appearInterval = setInterval(()=> {
-        opacity += increment;
-        first.style.opacity = opacity;
-
-            if (opacity >= 1) {
-                 clearInterval(appearInterval);
-            }
-         }, duration * increment);
-
-         const secondAppearInterval = setInterval(() => {
-            opacity += increment;
-            second.style.opacity = opacity;
-
-            if (opacity >= 1){
-                clearInterval(secondAppearInterval);
-            }
-         }, duration * increment);
-     }
-
+// Function to toggle header images at intervals
+function toggleImage() {
+  if (firstHeaderImage.src.includes("Property_1_Variant1.png")) {
+    firstHeaderImage.src = "images/Modern cute chatbot illustration set 3.png";
+  } else {
+    firstHeaderImage.src = "images/Property_1_Variant1.png";
+  }
 }
-updateImages();
-moveRight();
-appearInterval();
-secondAppearInterval();
+
+function imageToggle() {
+  if (secondHeaderImage.src.includes("Property_1_Variant2.png")) {
+    secondHeaderImage.src = "images/Modern cute chatbot illustration set 4.png";
+  } else {
+    secondHeaderImage.src = "images/Property_1_Variant2.png";
+  }
+}
+
+setInterval(toggleImage, 1000);
+setInterval(imageToggle, 1000);
+
+// Function to move a line to the right
+moveRight = () => {
+  let position = 0;
+
+  function animate() {
+    if (position == 240) {
+      clearInterval(animation);
+    } else {
+      position++;
+      vl.style.left = position + "px";
+    }
+  }
+
+  const animation = setInterval(animate, 15);
+}
+
+// Function to make text appear with fading effect
+appearText = (text) =>{
+  const duration = 2000;
+  const increment = 0.01;
+  let opacity = 0;
+
+  const appearInterval = setInterval(() => {
+    opacity += increment;
+    text.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(appearInterval);
+    }
+  }, duration * increment);
+}
+
+window.addEventListener("load", () => {
+  // Call the moveRight and appearText functions after the window is loaded
+  moveRight();
+  appearText(first);
+});
